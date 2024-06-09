fa4a6384e7191b99ec69382f1d6131f83ad552ea
Kévin MAZE <kekeproject@hotmail.fr>
2024-06-09 16:51:54 +0200

    change page card

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import ListArticle from './Components/ListArticle';
 
+const fakeData = [
+  { name : 'Margueritas', price : '30'},
+  { name : '4 Fromages', price : '32'},
+  { name : 'Pamplemousse'},
+  { name : '4 Saisons', price : '58'}
+]
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Salut la mif!! sa marche bordel tjrs.
+          <ListArticle articles={fakeData}/>
+
         </p>
         <a
           className="App-link"
---
+++ src/Components/ListArticle.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import CardArticle from './CardArticle';
+
+
+function ListArticle (props) {
+    if (!props) { return <div><h1>Pas d'article(s)</h1></div>}
+    else {
+        const articlesListing = props.articles.map((article) => {
+            if(article.price) return <CardArticle name={article.name} price={article.price}/>
+            else return null
+        })
+            return (
+                <div>
+                    {articlesListing}
+                </div>
+            )
+        }
+    }
+
+    export default ListArticle;
